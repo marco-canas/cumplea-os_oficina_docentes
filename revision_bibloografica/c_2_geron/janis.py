7eff49bfcd1b055a1f8362aebc04b9ea43eb8ea9
Marco Julio Cañas Campillo <66282835+marco-canas@users.noreply.github.com>
2022-10-07 21:35:47 -0500

    dirección del cuaderno cap 2 de geron
    adición de dirección a archivo .py

--- revision_bibloografica/c_2_geron/janis.py
+++ revision_bibloografica/c_2_geron/janis.py
@@ -1,5 +1,6 @@
 # En este archivo consignaremos las funciones que para la primera parte de construcción de un modelo de regresión se requieren 
 # En especial las funciones necesarias para el trabajo en las páginas 67 a 80. 
+# https://github.com/ageron/handson-ml3/blob/main/02_end_to_end_machine_learning_project.ipynb
 
 def obtener_datos(datos_csv):
     from pathlib import Path
