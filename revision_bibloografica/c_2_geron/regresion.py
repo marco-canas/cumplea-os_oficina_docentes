652d4bbdf20baf0e21e85f80893ab47ec861421c
Marco Julio Cañas Campillo <66282835+marco-canas@users.noreply.github.com>
2022-10-10 15:11:16 -0500

    actualizacion 10 de octubre

--- revision_bibloografica/c_2_geron/regresion.py
+++ revision_bibloografica/c_2_geron/regresion.py
@@ -2,22 +2,12 @@
 # En especial las funciones necesarias para el trabajo en las páginas 67 a 80. 
 # https://github.com/ageron/handson-ml3/blob/main/02_end_to_end_machine_learning_project.ipynb
 
-def obtener_datos(datos_csv):
+def obtener_datos(datos_url):
     '''
     # documentación de esta función 
     '''
-    from pathlib import Path
     import pandas as pd
-    import tarfile
-    import urllib.request
-    tarball_path = Path("datasets/housing.tgz")
-    if not tarball_path.is_file():
-        Path("datasets").mkdir(parents=True, exist_ok=True)
-        url = "https://github.com/ageron/data/raw/main/housing.tgz"
-        urllib.request.urlretrieve(url, tarball_path)
-        with tarfile.open(tarball_path) as housing_tarball:
-            housing_tarball.extractall(path="datasets")
-    return pd.read_csv(Path("datasets/housing/housing.csv"))
+    return pd.read_csv(datos_url)
 
 
 
